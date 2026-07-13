bad104d38d669bd15721802a8ea3a62a7f998e40
Haja <hajatiana.rabemananjara@epitech.eu>
2026-07-13 15:00:52 +0200

    feat(docker): complete compose stack with server, db, desktop build and web

--- server/src/main.rs
+++ server/src/main.rs
@@ -35,6 +35,10 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     tracing::info!("Connected to PostgreSQL");
 
+    sqlx::migrate!("./migrations").run(&pool).await?;
+    tracing::info!("Migrations applied");
+    
+
     let registry = ReactionRegistry::builder()
         .register(Arc::new(VigilCreateIncident::new()))
         .register(Arc::new(VigilEscalateIncident::new()))
@@ -79,6 +83,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             "http://localhost:3000".parse::<HeaderValue>().unwrap(),
             "tauri://localhost".parse::<HeaderValue>().unwrap(),
             "http://localhost:9527".parse::<HeaderValue>().unwrap(),
+            "http://localhost:8081".parse::<HeaderValue>().unwrap(),
             ])
         .allow_methods([Method::GET, Method::POST, Method::PATCH, Method::DELETE])
         .allow_headers([
